43f8ebe7b27f1b427df507a5290e3de991e9d908
Esar Behlum <esarnb@gmail.com>
2019-08-04 11:52:03 -0700

    force empty db

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ if (process.env.NODE_ENV === "production") {
 
 // Start the API server
 var db = require("./models");
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
 	  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 	});
