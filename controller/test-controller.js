1154b64d59f860c3bb8ce054e64ba06492a4ad7c
Esar Behlum <esarnb@gmail.com>
2019-08-05 18:17:08 -0700

    commitname

--- controller/test-controller.js
+++ controller/test-controller.js
@@ -3,20 +3,20 @@
 module.exports = {
   findAll: function(req, res) {
     console.log("RECEIVED CLIENT GET REQUEST");
-    res.json({status: 200, message: "GET received"})
+    res.json({status: 200, message: "GET COOOKIES"})
   },
   create: function(req, res) {
     console.log("RECEIVED CLIENT POST REQ.BODY: ", req.body);
-    res.json({status: 200, msg: "POST received", req: req.body})
+    res.json({status: 200, msg: "POST COOKIES", req: req.body})
   },
   
   update: function(req, res) {
     console.log("RECEIVED CLIENT PUT REQ.PARAMS: ", req.params);
-    res.json({status: 200, msg: `Put received. Id: ${req.params.id}`})
+    res.json({status: 200, msg: `Put COOKIES. Id: ${req.params.id}`})
   },
   
   remove: function(req, res) {
     console.log("RECEIVED CLIENT DELETE REQ.PARAMS: ", req.params);
-    res.json({status: 200, msg: `Delete received. Id: ${req.params.id}`})
+    res.json({status: 200, msg: `Delete PRETZELS. Id: ${req.params.id}`})
   },
 }
